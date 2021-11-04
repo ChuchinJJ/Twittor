5e58525bd9688bb20ac7d6aa350b35329aba6eb2
ChuchinJJ <josedejesuscons@gmail.com>
2021-11-03 19:29:39 -0600

    primer commit

---
+++ js/sw-utils.js
@@ -0,0 +1,10 @@
+function actualizaDinamico(dynamicCache, req, res){
+  if(res.ok){
+    return caches.open(dynamicCache).then(cache=>{
+      cache.put(req, res.clone());
+      return res.clone();
+    });
+  }else{
+    return res;
+  }
+}
